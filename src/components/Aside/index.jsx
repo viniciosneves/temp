5db5d0229d9954d3728856df28440437ff5077cb
Vinicios Neves <vinicios.neves@mac>
2024-03-27 16:29:40 +0000

    Mão na massa: menu Aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -3,12 +3,51 @@ import styles from './aside.module.css'
 
 import logo from './logo.png'
 import Link from 'next/link'
+import AsideLink from '../AsideLink'
+
+import { Feed } from '../icons/Feed'
+import { Account } from '../icons/Account'
+import { Info } from '../icons/Info'
+import { Login } from '../icons/Login'
+import { Button } from '../Button'
 
 export const Aside = () => {
     return (<aside className={styles.aside}>
-        {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-        <Link href="/">
-            <Image src={logo} alt="Logo da Code Connect"/>
-        </Link>
+        <ul>
+            <li>
+                <Link href="/">
+                    <Image src={logo} alt="Logo da Code Connect" />
+                </Link>
+            </li>
+            <li>
+                <Button href="/publish" outline>
+                    Publicar
+                </Button>
+            </li>
+            <li>
+                <AsideLink href="/">
+                    <Feed />
+                    Feed
+                </AsideLink>
+            </li>
+            <li>
+                <AsideLink href="/profile">
+                    <Account />
+                    Perfil
+                </AsideLink>
+            </li>
+            <li>
+                <AsideLink href="/about">
+                    <Info />
+                    Sobre nós
+                </AsideLink>
+            </li>
+            <li>
+                <AsideLink href="/login?">
+                    <Login />
+                    Login
+                </AsideLink>
+            </li>
+        </ul>
     </aside>)
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,18 @@
-import styles from './button.module.css'
+import styles from './button.module.css';
+import Link from 'next/link';
 
-export const Button = ({children}) => {
-    return <button className={styles.btn}>
-        {children}
-    </button>
+export const Button = ({ children, outline, href }) => {
+    if (href) {
+        return (
+            <Link href={href} className={outline ? styles.outline : styles.btn}>
+                {children}
+            </Link>
+        );
+    }
+
+    return (
+        <button className={outline ? styles.outline : styles.btn}>
+            {children}
+        </button>
+    );
 }
