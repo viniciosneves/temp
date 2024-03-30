f2331b8d8f5cdf3732757ec5e64ac03b39586cb3
Vinicios Neves <vinicios.neves@mac>
2024-03-30 19:33:57 +0000

    video 4.3

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -12,57 +12,65 @@ import { Providers } from '@/components/Providers'
 import Link from 'next/link'
 import { Login } from '@/components/icons/Login'
 import { createUser } from '@/actions'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 export default async function Signup() {
-    return (
-      <main className={styles.main}>
-        <div>
-          <Image src={banner} alt='Banner' priority/>
-        </div>
-        <div className={styles.container}>
-          <h1>
-            Cadastro
-          </h1>
-          <h2>
-            Olá! Preencha seus dados.
-          </h2>
-          <form className={styles.form} action={createUser}>
-            <div>
-              <Label>
-                Nome
-              </Label>
-              <Input name="name" id="name" placeholder="Nome completo" required />
-            </div>
-            <div>
-              <Label>
-                E-mail
-              </Label>
-              <Input name="email" id="email" type="email" placeholder="Digite seu e-mail" required />
-            </div>
-            <div>
-              <Label>
-                Senha
-              </Label>
-              <Input name="password" id="password" type="password" required/>
-              <Checkbox label="Li e aceito os termos de uso" required/>
-            </div>
-            <div className={styles.action}>
-              <Button type="submit">
-                Cadastrar <ArrowFoward />
-              </Button>
-            </div>
-          </form>
-          <div className={styles.providers}>
-            <TextDivider text="ou entre com outras contas" />
-            <Providers />
-          </div>
-          <footer className={styles.footer}>
-            <p>
-              Já tem conta? <Link href='/api/auth/signin'>Faça seu login! <Login color="#81FE88"/></Link>
-            </p>
-          </footer>
-        </div>
-      </main>
-    )
+
+  const session = await getServerSession()
+
+  if (session) {
+    redirect("/posts");
   }
-  
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <Image src={banner} alt='Banner' priority />
+      </div>
+      <div className={styles.container}>
+        <h1>
+          Cadastro
+        </h1>
+        <h2>
+          Olá! Preencha seus dados.
+        </h2>
+        <form className={styles.form} action={createUser}>
+          <div>
+            <Label>
+              Nome
+            </Label>
+            <Input name="name" id="name" placeholder="Nome completo" required />
+          </div>
+          <div>
+            <Label>
+              E-mail
+            </Label>
+            <Input name="email" id="email" type="email" placeholder="Digite seu e-mail" required />
+          </div>
+          <div>
+            <Label>
+              Senha
+            </Label>
+            <Input name="password" id="password" type="password" required />
+            <Checkbox label="Li e aceito os termos de uso" required />
+          </div>
+          <div className={styles.action}>
+            <Button type="submit">
+              Cadastrar <ArrowFoward />
+            </Button>
+          </div>
+        </form>
+        <div className={styles.providers}>
+          <TextDivider text="ou entre com outras contas" />
+          <Providers />
+        </div>
+        <footer className={styles.footer}>
+          <p>
+            Já tem conta? <Link href='/api/auth/signin'>Faça seu login! <Login color="#81FE88" /></Link>
+          </p>
+        </footer>
+      </div>
+    </main>
+  )
+}
--- src/app/(blog)/profile/page.js
+++ src/app/(blog)/profile/page.js
@@ -1,10 +1,16 @@
 import { ProfileAvatar } from "@/components/ProfileAvatar"
 import { getServerSession } from "next-auth"
 import db from "../../../../prisma/db"
+import { redirect } from "next/navigation";
 
 export default async function Profile () {
 
     const session = await getServerSession()
+
+    if (!session) {
+        redirect("/api/auth/signin?callbackUrl=/profile");
+    }
+
     const user = await db.user.findFirst({ where: {
         email: session.user.email
     } })
